d1c41029c818a16986abd015b7c643757de20fda
cozin101 <joalozano@proton.me>
2024-06-21 13:25:24 -0300

    Agregando espacios y cambiando la presentación del archivo

--- TP2/tp2.pl
+++ TP2/tp2.pl
@@ -13,19 +13,19 @@ tablero(X, Y, [T|Ts]):- X > 0, length(T, Y), X1 is X-1, tablero(X1, Y, Ts).
 %% Ejercicio 2
 %% ocupar(+Pos,?Tablero) será verdadero cuando la posición indicada esté ocupada.
 ocupar(pos(X, Y), Ts) :- nonvar(Ts), iesimo(X, Ts, Fila), iesimo(Y, Fila, ocupada).
-ocupar(pos(X, Y), Ts) :- var(Ts), X1 is X+1, Y1 is Y+1, tablero(X1, Y1, Ts), ocupar(pos(X, Y), Ts).
+ocupar(pos(X, Y), Ts) :- var(Ts), X1 is X + 1, Y1 is Y + 1, tablero(X1, Y1, Ts), ocupar(pos(X, Y), Ts).
 
-%% iesimo(+N, ?L, ?X) vale cuando X está en la posición 'i' de la lista L.
+%% iesimo(+N, ?L, ?X) vale cuando X está en la posición 'I' de la lista L.
 iesimo(0, [T | _], T).
-iesimo(N, [_ | Ts], Fila) :- N > 0, N1 is N-1, iesimo(N1, Ts, Fila).
+iesimo(I, [_ | Ts], Fila) :- I > 0, I1 is I - 1, iesimo(I1, Ts, Fila).
 
 %% Ejercicio 3
 %% vecino(+Pos, +Tablero, -PosVecino) será verdadero cuando PosVecino sea
 %% un átomo de la forma pos(F', C') y pos(F',C') sea una celda contigua a
 %% pos(F,C), donde Pos=pos(F,C). Las celdas contiguas puede ser a lo sumo cuatro
 %% dado que el robot se moverá en forma ortogonal.
-vecino(pos(X,Y),[T|Ts], pos(X1, Y1)) :- member(dir(N, M),[dir(1, 0),dir(-1, 0),dir(0, 1), dir(0,-1)]), 
-    X1 is X+N, Y1 is Y+M, length([T | Ts], F), length(T, C), F1 is F-1, C1 is C-1,
+vecino(pos(X,Y),[T | Ts], pos(X1, Y1)) :- member(dir(N, M),[dir(1, 0), dir(-1, 0), dir(0, 1), dir(0,-1)]), 
+    X1 is X + N, Y1 is Y + M, length([T | Ts], F), length(T, C), F1 is F - 1, C1 is C - 1,
     between(0, F1, X1), between(0, C1, Y1).
 
 %% Ejercicio 4
@@ -70,9 +70,8 @@ caminoAux(Inicio, Fin, T, [Vecino | Camino], Visitados) :- Inicio \= Fin, vecino
 %% camino2(+Inicio, +Fin, +Tablero, -Camino) ídem camino/4 pero que las soluciones
 %% se instancien en orden creciente de longitud.
 camino2(Inicio, Fin, [T | Ts], Camino) :- length([T | Ts], Fila), length(T, Columna),
-    Longmaximo is  Fila * Columna,
-    between(0, Longmaximo, Len), camino(Inicio, Fin, [T | Ts], Camino),
-    length(Camino, Len).
+	Longmaximo is  Fila * Columna, between(0, Longmaximo, Len),
+	camino(Inicio, Fin, [T | Ts], Camino), length(Camino, Len).
 
 %% 6.1. Analizar la reversibilidad de los parámetros Inicio y Camino justificando adecuadamente en
 %% cada caso por qué el predicado se comporta como lo hace.
@@ -81,7 +80,8 @@ camino2(Inicio, Fin, [T | Ts], Camino) :- length([T | Ts], Fila), length(T, Colu
 %% Ejercicio 7
 %% caminoOptimo(+Inicio, +Fin, +Tablero, -Camino) será verdadero cuando Camino sea un
 %% camino óptimo sobre Tablero entre Inicio y Fin. Notar que puede no ser único.
-caminoOptimo(Inicio, Fin, T, C) :- camino(Inicio, Fin, T, C), length(C, X), not(otroCamino(Inicio, Fin, T, X)).
+caminoOptimo(Inicio, Fin, T, C) :- camino(Inicio, Fin, T, C), length(C, X),
+	not(otroCamino(Inicio, Fin, T, X)).
 
 %% otroCamino(+Inicio, +Fin, +Tablero, -LongitudÓptima) sólo es verdad cuando existe algún camino
 % cuya longitud sea menor  que la del camino óptimo.
@@ -104,13 +104,13 @@ caminoDual(Inicio, Fin, T1, T2, C) :- camino(Inicio, Fin, T1, C), camino(Inicio,
 tablero(tests,T) :- tablero(3,3,T), ocupar(pos(0,1),T), ocupar(pos(1,1),T).
 
 cantidadTestsTablero(8). % Actualizar con la cantidad de tests que entreguen
-testTablero(1) :- tablero(0,0,[]).
-testTablero(2) :- tablero(3,2,[[_,_],[_,_],[_,_]]).
-testTablero(3) :- tablero(2,3,[[_,_,_],[_,_,_]]).
-testTablero(4) :- not(tablero(2,2,[[_,_,_],[_,_]])).
-testTablero(5) :- not(tablero(2,2,[[_,_],[_,_],[_,_]])).
-testTablero(6) :- ocupar(pos(0,0), [[ocupada]]).
-testTablero(7) :- tablero(tests,T), ocupar(pos(0,1), T). % vale checkear si está ocupado un casillero.
+testTablero(1) :- tablero(0, 0, []).
+testTablero(2) :- tablero(3, 2, [[_, _], [_, _], [_, _]]).
+testTablero(3) :- tablero(2, 3, [[_, _, _], [_, _, _]]).
+testTablero(4) :- not(tablero(2, 2, [[_, _, _],[_, _]])).
+testTablero(5) :- not(tablero(2, 2, [[_, _], [_, _],[_, _]])).
+testTablero(6) :- ocupar(pos(0, 0), [[ocupada]]).
+testTablero(7) :- tablero(tests,T), not(noOcupada(pos(0,1), T)). % vale checkear si está ocupado un casillero.
 testTablero(8) :- tablero(tests,T), noOcupada(pos(0,0), T). % vale checkear si no está ocupado.
 % Agregar más tests
 
@@ -119,13 +119,13 @@ testVecino(1) :- tablero(2,2,T), vecino(pos(0,0), T, pos(0,1)), vecino(pos(0,0),
 testVecino(2) :- tablero(3,3,T), vecino(pos(1,1), T, pos(1,0)), vecino(pos(1,1), T, pos(0,1)),
 	vecino(pos(1,1), T, pos(2,1)), vecino(pos(1,1), T, pos(1,0)). % es posible irse en todas las direcciones
 testVecino(3) :- not(vecino(pos(0,0), [[_]], pos(0,1))), not(vecino(pos(0,0), [[_]], pos(1,0))). % no se va del tablero
-testVecino(4) :- not(vecino(pos(0,0), [[_,_],[_,_]], pos(1,1))). % no se mueve en diagonal
-testVecino(5) :- not(vecino(pos(1,1), [[_,_],[_,_]], pos(0,0))). % no se mueve en diagonal II
-testVecino(6) :- not(vecino(pos(0,0), [[_,_],[_,_]], pos(0,0))). % la posición actual no es vecino
-testVecino(7) :- vecino(pos(0,0), [[_,_],[ocupada,_]], pos(1,0)). % las posiciones ocupadas son vecinos
+testVecino(4) :- not(vecino(pos(0,0), [[_, _],[_, _]], pos(1,1))). % no se mueve en diagonal
+testVecino(5) :- not(vecino(pos(1,1), [[_, _],[_, _]], pos(0,0))). % no se mueve en diagonal II
+testVecino(6) :- not(vecino(pos(0,0), [[_, _],[_, _]], pos(0,0))). % la posición actual no es vecino
+testVecino(7) :- vecino(pos(0,0), [[_, _],[ocupada, _]], pos(1,0)). % las posiciones ocupadas son vecinos
 testVecino(8) :- tablero(2,2,T), vecinoLibre(pos(0,0), T, pos(1,0)), vecinoLibre(pos(0,0), T, pos(0,1)).
 testVecino(9) :- tablero(2,2,T), ocupar(pos(0,1),T), not(vecinoLibre(pos(0,0), T, pos(0,1))). % los vecinos ocupados no son libres
-testVecino(10) :- tablero(2,2,T), ocupar(pos(0,1),T), ocupar(pos(1,0), T), not(vecinoLibre(pos(0,0),T,_)). % los hay vecinos libres
+testVecino(10) :- tablero(2,2,T), ocupar(pos(0,1),T), ocupar(pos(1,0), T), not(vecinoLibre(pos(0,0),T, _)). % los hay vecinos libres
 % Agregar más tests
 
 cantidadTestsCamino(13). % Actualizar con la cantidad de tests que entreguen
